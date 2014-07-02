5678215aa43b70e0edb113aaf69261a2458fd70e
Nicole Aitchison <nlaitchison@gmail.com>
2014-07-02 11:02:36 -0400

    copied all my files over

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+
+	// Smooth scroll
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
+	// Slide Header on click
+	var headerSlide = false;
+
+	$('#logo').click(function() {
+		if(headerSlide){
+			headerSlide = false;
+		}else{
+			headerSlide = true;
+		}
+		$('#header-slider').toggleClass('slide', headerSlide);
+	});
+
+});
+
